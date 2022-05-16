0dbb4932c3d3614496171f67c4437d863a1e4f1a
JoshZheng <6106josh@gmail.com>
2022-05-16 15:14:09 +0800

    選擇模式後跳出該模式的背景

--- src/main/java/com/example/runningcat/Choose_difficulty.java
+++ src/main/java/com/example/runningcat/Choose_difficulty.java
@@ -1,11 +1,14 @@
 package com.example.runningcat;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -18,29 +21,41 @@ public class Choose_difficulty {
     private Stage stage;
     private Scene scene;
     private Parent root;
+    private ImageView easyBG;
+    private ImageView mediumBG;
+    private ImageView hardBG;
     public void easy_mode(ActionEvent event) throws IOException {
         System.out.println("easy");
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("road-easy-view.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Image myImage = new Image(getClass().getResourceAsStream("easy1.jpg"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        easyBG.setImage(myImage);
+
     }
+
     public void medium_mode(ActionEvent event) throws IOException {
         System.out.println("medium");
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("road-medium-view.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Image myImage = new Image(getClass().getResourceAsStream("medium1.jpg"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        mediumBG.setImage(myImage);
     }
+
     public void hard_mode(ActionEvent event) throws IOException {
         System.out.println("hard");
         Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("road-hard-view.fxml")));
-        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Image myImage = new Image(getClass().getResourceAsStream("hard1.jpg"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        hardBG.setImage(myImage);
     }
 
 
