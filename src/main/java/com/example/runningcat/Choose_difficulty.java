c5af8255da6f227ef1c3d9cc98bec84a83861822
海狸大師 <77geo5rge6@gmail.com>
2022-05-17 21:15:13 +0800

    把衝突的code做處理

--- src/main/java/com/example/runningcat/Choose_difficulty.java
+++ src/main/java/com/example/runningcat/Choose_difficulty.java
@@ -19,17 +19,14 @@ import java.util.Objects;
 
 
 public class Choose_difficulty {
-    // 這邊是要判定生成不同大小的障礙物的class，println的地方之後改成傳送給障礙物大小那邊的信號
 
+    // 這邊是要判定生成不同大小的障礙物的class，println的地方之後改成傳送給障礙物大小那邊的信號
     private Stage stage;
     private Scene scene;
     Cones_Controller cones_controller = new Cones_Controller();
-
     FXMLLoader loader = new FXMLLoader(getClass().getResource("road-easy-view.fxml"));
-
     public SetCatPos SP = new SetCatPos();
-
-    CatMovement catMovement = loader.getController();
+    CatMovement catMovement = loader.getController(); // 還沒打完
 
 
     public void easy_mode(ActionEvent event) throws IOException {
@@ -39,12 +36,10 @@ public class Choose_difficulty {
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+        // 生成三角錐在視窗頂部的隨機位置
+        root.getChildren().add(cones_controller.Cones_Appear("easy_mode"));
         root.getChildren().add(SP.catImageView());
         scene.setOnKeyPressed(new EventHandler<KeyEvent>(){
-
-        // 生成三角錐在視窗頂部的隨機位置
-
-        root.getChildren().add(cones_controller.Cones_Appear("easy_mode"));
             @Override
             public void handle(KeyEvent event) {
                 System.out.println(event.getCode());
@@ -73,6 +68,4 @@ public class Choose_difficulty {
         root.getChildren().add(cones_controller.Cones_Appear("hard_mode"));
         root.getChildren().add(SP.catImageView());
     }
-
-
 }
